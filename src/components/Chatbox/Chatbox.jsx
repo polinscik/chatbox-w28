f1f6aeaecee0806bc7101ccf7306586132d4a3ff
Polina <polinscik@gmail.com>
2023-11-10 20:56:55 +0300

    feat!: validation, styling

--- src/components/Chatbox/Chatbox.jsx
+++ src/components/Chatbox/Chatbox.jsx
@@ -1,18 +1,42 @@
 import React from "react";
+import "./Chatbox.scss";
 
 export default function Chatbox({chatLogs}) {
   const reversedLogs = [...chatLogs];
   reversedLogs.reverse();
   return (
-    <div>
-      <h1>Chatbox</h1>
+    <div className="chatbox-wrapper">
       <div className="chatbox">
         {reversedLogs.map((log) => (
-          <div key={log.id}>
-            {log.username} said {log.message}
+          <div
+            className={
+              log.id == chatLogs.length - 1 ? "new chatlog" : "chatlog"
+            }
+            key={log.id}>
+            <div className="chatlog__userInfo">
+              <img
+                className="chatlog__userImage"
+                src={log.avatar}
+                alt="avatar"
+              />
+              <p className="chatlog__userName"> {log.username}</p>
+              <p className="chatlog__time">{log.date}</p>
+            </div>
+            <div className="chatlog__message">{log.message}</div>
           </div>
         ))}
       </div>
     </div>
   );
+}
+
+{
+  /* <div class="chatlog">
+  <div class="chatlog__userInfo">
+<img class="chatlog__userImage" src="" alt="" />
+<p class="chatlog__userName"></p>
+<p class="chatlog__time"></p>
+</div>
+<div class="chatlog__message"></div>
+</div> */
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -9,6 +9,11 @@ export default function Form({setChatLogs, chatLogs}) {
     message: "",
   });
 
+  const [usernameInvalid, setUsernameInvalid] = useState(false);
+  const [messageInvalid, setMessageInvalid] = useState(false);
+  const usernameClassname = usernameInvalid ? "invalid" : "";
+  const messageClassname = messageInvalid ? "invalid" : "";
+
   const handleChange = (e) => {
     const type = e.target.type;
     if (type === "checkbox") {
@@ -18,45 +23,63 @@ export default function Form({setChatLogs, chatLogs}) {
           username: "Аноним",
           [e.target.name]: e.target.checked,
         });
+      } else if (formData.isAnon) {
+        setFormData({
+          ...formData,
+          username: "",
+          [e.target.name]: e.target.checked,
+        });
       } else setFormData({...formData, [e.target.name]: e.target.checked});
     } else {
-      setFormData({...formData, [e.target.name]: e.target.value.trim()});
+      setFormData({...formData, [e.target.name]: e.target.value});
+      if (e.target.value.trim()) {
+        if (e.target.name === "username") {
+          setUsernameInvalid(false);
+        }
+        if (e.target.name === "message") {
+          setMessageInvalid(false);
+        }
+      }
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (formData.username == "" || formData.message == "") {
-      console.log("invalid");
+    if (formData.username === "") {
+      setUsernameInvalid(true);
+    } else if (formData.message === "") {
+      setMessageInvalid(true);
     } else {
-      console.log(formData);
       setChatLogs([
         ...chatLogs,
         {
-          username: formData.username,
+          username: makeFirstLetterBig(formData.username.trim()),
           isAnon: formData.isAnon,
-          avatar: formData.avatar,
-          message: formData.message,
+          avatar: validateUrl(formData.avatar),
+          message: checkSpam(formData.message.trim()),
           id: chatLogs.length,
+          date: getTimeAndDate(),
         },
       ]);
+      setFormData({...formData, message: ""});
     }
   };
 
   return (
     <div>
-      <h1>Form</h1>
+      <h1>Чат со спам-фильтром</h1>
       <form noValidate className="form" onSubmit={(e) => handleSubmit(e)}>
-        <div>
-          <label htmlFor="username">Введите имя: </label>
+        <div className="input-wr">
+          <label htmlFor="username">Введите имя*: </label>
           <input
             value={formData.username}
             onChange={(e) => handleChange(e)}
             name="username"
             type="text"
+            className={usernameClassname}
           />
         </div>
-        <div>
+        <div className="input-wr">
           <label htmlFor="isAnon">Отправить анонимно: </label>
           <input
             value={formData.isAnon}
@@ -65,7 +88,7 @@ export default function Form({setChatLogs, chatLogs}) {
             type="checkbox"
           />
         </div>
-        <div>
+        <div className="input-wr">
           <label htmlFor="avatar">
             Введите ссылку на аватар (необязательно):{" "}
           </label>
@@ -76,17 +99,73 @@ export default function Form({setChatLogs, chatLogs}) {
             type="url"
           />
         </div>
-        <div>
-          <label htmlFor="message">Введите текст сообщения: </label>
+        <div className="input-wr">
+          <label htmlFor="message">Введите текст сообщения*: </label>
           <textarea
             value={formData.message}
             onChange={(e) => handleChange(e)}
             name="message"
             type="text"
+            className={messageClassname}
+            cols="30"
+            rows="10"
           />
         </div>
+        {(messageInvalid || usernameInvalid) && (
+          <p className="error">Заполните нужные поля</p>
+        )}
         <button type="submit">Отправить</button>
+        {/* type= button? */}
       </form>
     </div>
   );
 }
+
+let options = {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+};
+
+function checkSpam(string) {
+  let stringChecked;
+  stringChecked = string.replace(/viagra/gim, "***");
+  stringChecked = stringChecked.replace(/xxx/gim, "***");
+  return stringChecked;
+}
+function makeFirstLetterBig(string) {
+  let newString =
+    string.toUpperCase().slice(0, 1) + string.slice(1).toLowerCase();
+  return newString;
+}
+
+function getTimeAndDate() {
+  const date = new Date();
+  const viewDate = date.toLocaleString("ru", options);
+  return viewDate;
+}
+
+function assignRandomAvatar() {
+  const randomAvatar =
+    defaultAvatars[Math.floor(Math.random() * defaultAvatars.length)];
+  return randomAvatar;
+}
+
+function validateUrl(url) {
+  const regex =
+    /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
+  if (regex.test(url)) {
+    return url;
+  } else return assignRandomAvatar();
+}
+
+const defaultAvatars = [
+  "/src/assets/images/pfp-butterflies.jpg",
+  "/src/assets/images/pfp-cat.jpg",
+  "/src/assets/images/pfp-clouds.jpg",
+  "/src/assets/images/pfp-devil.jpg",
+  "/src/assets/images/pfp-orange-sky.jpg",
+  "/src/assets/images/pfp-sunset.jpg",
+];
